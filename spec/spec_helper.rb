04ad9c5a0d1f7f4d12a52668272535700e741823
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-23 14:31:01 +0000

    fixes database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,9 +24,21 @@ Capybara.app = BookmarkManager
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  
+
+  # Everything in this block runs once before all the tests run
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
   config.before(:each) do
     DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
