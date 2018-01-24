94a525cf516a4dfe21ae842fa5876c36c2e134ae
Ed Perkins <ed@edperkins.co.uk>
2018-01-24 16:42:01 +0000

    filter by tag

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -6,4 +6,14 @@ feature 'Viewing links on the homepage' do
       expect(page).to have_content('HTML5 Drum Machine')
     end
   end
-end
+
+  scenario 'Links can be viewed by tag' do
+    Link.create(url: "http://www.html5drummachine.com/", title: "HTML5 Drum Machine", tags: [Tag.first_or_create(name: 'music')])
+    Link.create(url: "http://www.shooter-bubble.com/", title: "Bubble Shooter", tags: [Tag.first_or_create(name: 'bubbles')])
+    visit('/tags/bubbles')
+      within('ul#links') do
+        expect(page).not_to have_content('HTML5 Drum Machine')
+        expect(page).to have_content('Bubble Shooter')
+      end
+    end
+  end
