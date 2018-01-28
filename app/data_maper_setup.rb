e536bdefb243d41c61898e5245d0725ee5225807
Ed Perkins <ed@edperkins.co.uk>
2018-01-28 18:47:07 +0000

    readd datamapper spec

--- app/data_maper_setup.rb
+++ app/data_maper_setup.rb
@@ -1,10 +1,10 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
-require_relative 'models/tags'
 require_relative 'models/link'
 require_relative 'models/user'
+require_relative 'models/tags'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] ||"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
